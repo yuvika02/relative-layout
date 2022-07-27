4fcf560abd255ad8cf221fda8c2d265148db8c7d
yuvika02 <trikhayuvika20@gmail.com>
2022-07-27 21:28:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/assignment2/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.assignment2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative)
+    }
+}
